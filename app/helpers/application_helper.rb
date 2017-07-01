133a34da9eed91a6711b75770a3760a740b46d27
諦念 <teinen.github@gmail.com>
2017-07-01 23:15:03 +0900

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # ページごとの完全なタイトルを返却する
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+
+    if page_title.empty? # ページタイトルが空白の場合
+      base_title
+    else # ページタイトルが存在する場合
+      page_title + " | " + base_title
+    end
+  end
 end
